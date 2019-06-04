455fb391c2c5c77a28516d0b41ba6291e81a8ab5
luca-campanella <luca.campanella1@gmail.com>
2019-06-04 15:11:20 +0200

    ADNCUBA-103: add debug print for travis

--- graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
+++ graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
@@ -2,6 +2,7 @@ package com.github.lucacampanella.callgraphflows.staticanalyzer;
 
 import org.junit.jupiter.api.BeforeAll;
 
+import java.io.File;
 import java.net.URL;
 
 class JarAnalyzerTest extends AnalyzerWithModelTest {
@@ -10,6 +11,7 @@ class JarAnalyzerTest extends AnalyzerWithModelTest {
     static void setUp() {
         final URL testURL = JarAnalyzerTest.class.getClassLoader().getResource("test.txt");
         System.out.println(testURL);
+        System.out.println(new File(testURL.getFile()).toString() + ": " + new File(testURL.getFile()).exists());
         final URL jarURL = JarAnalyzerTest.class.getClassLoader().getResource("JarAnalyzerTestJar-1.0-SNAPSHOT.jar");
         analyzerWithModel = new JarAnalyzer(jarURL.getPath());
     }
