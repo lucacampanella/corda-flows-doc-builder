a945bf7c4e1eace767a9624ecf7352db33ddfd85
luca-campanella <luca.campanella1@gmail.com>
2019-06-04 15:06:46 +0200

    ADNCUBA-103: add a test print of a resoruce

--- graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
+++ graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
@@ -8,6 +8,8 @@ class JarAnalyzerTest extends AnalyzerWithModelTest {
 
     @BeforeAll
     static void setUp() {
+        final URL testURL = JarAnalyzerTest.class.getClassLoader().getResource("test.txt");
+        System.out.println(testURL);
         final URL jarURL = JarAnalyzerTest.class.getClassLoader().getResource("JarAnalyzerTestJar-1.0-SNAPSHOT.jar");
         analyzerWithModel = new JarAnalyzer(jarURL.getPath());
     }
