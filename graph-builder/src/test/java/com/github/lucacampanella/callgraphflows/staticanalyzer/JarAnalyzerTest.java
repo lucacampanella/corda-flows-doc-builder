213df9758e5c9c024a1259a437455a96fb1961b4
luca-campanella <luca.campanella1@gmail.com>
2019-06-05 11:27:14 +0200

    ADNCUBA-103: apply versioning scheme correctly

--- graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
+++ graph-builder/src/test/java/com/github/lucacampanella/callgraphflows/staticanalyzer/JarAnalyzerTest.java
@@ -12,7 +12,7 @@ class JarAnalyzerTest extends AnalyzerWithModelTest {
         final URL testURL = JarAnalyzerTest.class.getClassLoader().getResource("test.txt");
         System.out.println(testURL);
         System.out.println(new File(testURL.getFile()).toString() + ": " + new File(testURL.getFile()).exists());
-        final URL jarURL = JarAnalyzerTest.class.getClassLoader().getResource("JarAnalyzerTestJar-1.0-SNAPSHOT.jar");
+        final URL jarURL = JarAnalyzerTest.class.getClassLoader().getResource("JarAnalyzerTestJar.jar");
         System.out.println(jarURL);
         analyzerWithModel = new JarAnalyzer(jarURL.getPath());
     }
