42cdd9ba8343cae7db25e3a54c489a2e7a8429b8
Luca Campanella <luca.campanella1@gmail.com>
2019-06-25 15:36:21 +0200

    fix testing of plugin, create temp dir, copy, test there, it works also setting the gradle version

--- plugin/src/test/java/com/github/lucacampanella/plugin/FlowsDocBuilderPluginTest.java
+++ plugin/src/test/java/com/github/lucacampanella/plugin/FlowsDocBuilderPluginTest.java
@@ -4,6 +4,7 @@ import com.github.lucacampanella.TestUtils;
 import org.apache.commons.io.FileUtils;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,10 +15,12 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,19 +34,47 @@ class FlowsDocBuilderPluginTest {
     private static final File sampleProjectDirectory = Paths.get(upperDir.toString(), "simple-flow-project").toFile();
     private static final File outputDir = Paths.get(sampleProjectDirectory.toString(),
             "build", "reports", "differentdir", "flowsdocbuilder").toFile();
+    private static File tmpOutputdir = null;
+    private static File tmpDir = null;
+
+    private static final String[] filesToCopy = {"settings.gradle", "gradlew.bat", "gradlew", "build.gradle"};
+    private static final String[] dirsToCopy = {"gradle", "src"};
 
     @BeforeAll
     static void setUp() throws IOException {
+        tmpDir = Files.createTempDirectory("testTempDir").toFile();
 
-//        final File testTempDir = Files.createTempDirectory("testTempDir").toFile();
-//        LOGGER.trace(tmpDir);
-//
-//        FileUtils.copyDirectory(sampleProjectDirectory, tmpDir);
-//
-//        FileUtils.deleteDirectory(outputDir);
+        tmpOutputdir = Paths.get(tmpDir.toString(),
+                "build", "reports", "differentdir", "flowsdocbuilder").toFile();
 
-        final BuildResult buildResult = GradleRunner.create().withProjectDir(sampleProjectDirectory)
-                .withPluginClasspath().withArguments("JarAnalyzerTask").withGradleVersion("4.1").build();
+        LOGGER.trace("{}", tmpDir);
+
+        for(String dirName : dirsToCopy) {
+            FileUtils.copyDirectory(Paths.get(sampleProjectDirectory.getPath(), dirName).toFile(),
+                    Paths.get(tmpDir.getPath(), dirName).toFile());
+        }
+
+        for(String fileName : filesToCopy) {
+            FileUtils.copyFile(Paths.get(sampleProjectDirectory.getPath(), fileName).toFile(),
+                    Paths.get(tmpDir.getPath(), fileName).toFile());
+        }
+
+        //copy executable jar
+        final File[] candidateJarFiles = Paths.get("../graph-builder/build/libs").toFile()
+                .listFiles((dir, name) -> name.startsWith("graph-builder") && name.endsWith("-all.jar"));
+        File jarExecFile = null;
+        if(candidateJarFiles != null) {
+            jarExecFile = Arrays.stream(candidateJarFiles)
+                .findAny().orElseThrow(() -> new RuntimeException("Could not find local executable jar"));
+        } else { throw new RuntimeException("Could not find local executable jar"); }
+
+        FileUtils.copyFile(jarExecFile,
+                Paths.get(tmpDir.getPath(), jarExecFile.getName()).toFile());
+
+        //FileUtils.deleteDirectory(outputDir);
+
+        final BuildResult buildResult = GradleRunner.create().withProjectDir(tmpDir)
+                .withPluginClasspath().withArguments("JarAnalyzerTask")/*.withGradleVersion("4.10.1")*/.build();
 
         //todo: copy only the important files, not caches and so on, this may also allow to fire up different gradle
         //versions
@@ -54,7 +85,7 @@ class FlowsDocBuilderPluginTest {
 
     @Test
     void hasOutput() {
-        final File[] outputFiles = outputDir.listFiles();
+        final File[] outputFiles = tmpOutputdir.listFiles();
         assertThat(outputFiles).isNotEmpty();
     }
 
@@ -62,7 +93,7 @@ class FlowsDocBuilderPluginTest {
     void outputSVGIsCorrect() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
 
         //we check directly the XML file
-        final List<String> nodeContents = TestUtils.parseXMLFile(outputDir.toString()
+        final List<String> nodeContents = TestUtils.parseXMLFile(tmpOutputdir.toString()
                 + "/com.github.lucacampanella.testclasses.SimpleFlowTest$Initiator.svg");
 
         assertThat(nodeContents).hasSize(4);
@@ -77,9 +108,15 @@ class FlowsDocBuilderPluginTest {
     void outputAsciiDocIsCorrect() throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
 
         //we check directly the XML file
-        final File outFile = new File(outputDir.toString()
+        final File outFile = new File(tmpOutputdir.toString()
                 + "/com.github.lucacampanella.testclasses.SimpleFlowTest$Initiator.adoc");
 
         assertThat(outFile).exists();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        FileUtils.deleteDirectory(tmpDir);
+
     }
 }
