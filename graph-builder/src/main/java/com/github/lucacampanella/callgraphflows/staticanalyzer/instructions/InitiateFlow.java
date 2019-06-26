680390f0fdbabe799266726d2c75a6fb46ff9dcb
Luca Campanella <luca.campanella1@gmail.com>
2019-06-26 11:09:05 +0200

    Fix initiate flow wrong display of session

--- graph-builder/src/main/java/com/github/lucacampanella/callgraphflows/staticanalyzer/instructions/InitiateFlow.java
+++ graph-builder/src/main/java/com/github/lucacampanella/callgraphflows/staticanalyzer/instructions/InitiateFlow.java
@@ -1,15 +1,21 @@
 package com.github.lucacampanella.callgraphflows.staticanalyzer.instructions;
 
+import com.github.lucacampanella.callgraphflows.staticanalyzer.matchers.MatcherHelper;
 import com.github.lucacampanella.callgraphflows.utils.Utils;
 import com.github.lucacampanella.callgraphflows.staticanalyzer.AnalyzerWithModel;
 import com.github.lucacampanella.callgraphflows.staticanalyzer.StaticAnalyzerUtils;
-import spoon.reflect.code.CtAssignment;
-import spoon.reflect.code.CtLocalVariable;
-import spoon.reflect.code.CtStatement;
+import org.apache.commons.lang.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.reflect.code.*;
 
 import java.util.Optional;
 
 public class InitiateFlow extends InstructionStatement {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InitiateFlow.class);
+
+    String partyArgument = null;
 
     public InitiateFlow(CtStatement statement) {
         super(statement);
@@ -28,6 +34,15 @@ public class InitiateFlow extends InstructionStatement {
         }
         else if(statement instanceof CtAssignment) {
             initiateFlow.targetSessionName = Optional.ofNullable(((CtAssignment) statement).getAssigned().toString());
+        }
+
+        CtAbstractInvocation inv = MatcherHelper.getFirstMatchedExpression(statement, "initiateFlowMatcher");
+        try {
+            initiateFlow.partyArgument = inv.getArguments().get(0).toString();
+        } catch (NullPointerException e) {
+            LOGGER.warn("Couldn't find out the party name involved in the initiateFlow call {}, " +
+                    "continuing without this information, thy may affect analysis", statement);
+            LOGGER.trace("Exception: ", e);
         }
 
         initiateFlow.buildGraphElem();
@@ -56,12 +71,16 @@ public class InitiateFlow extends InstructionStatement {
     @Override
     public String getStringDescription() {
         StringBuilder sb = new StringBuilder();
-        sb.append("initiateFlow(");
         if(targetSessionName.isPresent()) {
             sb.append(Utils.removePackageDescriptionIfWanted(targetSessionName.get()));
+            sb.append(" = ");
+        }
+        sb.append("initiateFlow(");
+        if(partyArgument != null) {
+            sb.append(partyArgument);
         }
         else {
-            sb.append("session");
+            sb.append("<<Party>>");
         }
         sb.append(")");
 
